46cec0ca03793c184fb4b6246ea5cdc6258f73ae
m-janssens-boop <127706155+m-janssens-boop@users.noreply.github.com>
2023-04-04 15:19:20 -0700

    finished print statements

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,10 +2,10 @@ import os
 import csv
 
 #Path to collect data from the resource folder
-budget_data_csv = os.path.join('..' ,'Resources', 'budget_data_csv')
+budget_data_csv = os.path.join('.', 'PyBank','Resources', 'budget_data.csv')
 
 #Path to output data into analysis text document
-budget_data_analysis = os.path.join('..', 'analysis', 'budget_data_analysis.txt')
+budget_data_analysis = os.path.join('.', 'PyBank', 'analysis', 'budget_data_analysis.txt')
 
 #Set variables, lists, and counters
 #set month counter at 0
@@ -32,9 +32,9 @@ with open (budget_data_csv) as csvfile:
     #since there can be no change at the first month
     first_row = next(csvreader)
     #add one to  months counter to account for month in first row
-    total_months += 1
+    total_months = total_months + 1
     #add the total from the first row to the net total counter
-    net_total += int(first_row[1])
+    net_total = net_total + int(first_row[1])
     #set the first, previous net total as total in first row of data to use to find 
     #difference between it and next value to derive net change per month
     previous_net = int(first_row[1])
@@ -42,21 +42,49 @@ with open (budget_data_csv) as csvfile:
     for row in csvreader:
 
         #add one to month counter for each row
-        total_months += 1
+        total_months = total_months + 1
         #collect net profits/losses in net_total counter
-        net_total += int(row[1])
+        net_total = net_total + int(row[1])
 
         #add month of increase/decrease in list
         month_of_change.append(str(row[0]))
         #calculate net chgange between this month and last month and add it to list
-        #HOW DOES PYTHON KNOW THAT PREVIOUS NET CHANGES PER ROW?
-        net_change_list.append(previous_net-int(row[1]))
+        net_change_list.append(int(row[1]) - previous_net)
         #change the value of previous net to the value of current row
         previous_net = int(row[1])
 
         #compare value of net change list to that held in greatest increase to see 
-        # if it should append value or not
-
+        # if it should append value or not and add the month associated with increase
+        #to list
+        if net_change_list[-1] > greatest_increase[1]:
+           greatest_increase[1] = net_change_list[-1]
+           greatest_increase[0] = month_of_change[-1]
 
         #compare value of net change list to that held in greatest decrease to see 
-        # if it should append value or not
+        # if it should append value or not and add the month associated with decrease
+        #to list
+        if net_change_list[-1] < greatest_decrease[1]:
+           greatest_decrease[1] = net_change_list[-1]
+           greatest_decrease[0] = month_of_change[-1]
+    
+    #Find the average of the net_change_list
+    net_change_average = round(sum(net_change_list)/len(net_change_list), 2)
+
+    #Print header
+    print("Financial Analysis\n------------------------------")
+
+    #Print total months
+    print(f"Total Months:{total_months}")
+
+    #Print net total
+    print(f"Total :${net_total}")
+
+    #Print average of net_change_list
+    print(f"Average Change :${net_change_average}")
+
+    #Print Greatest increase in profits
+    print(f"Greatest Increase in Profits:{greatest_increase[0]} ${greatest_increase[1]}")
+
+    #Print greatest decrease in profits
+    print(f"Greatest Decrease in Profits:{greatest_decrease[0]} ${greatest_decrease[1]}")
+
